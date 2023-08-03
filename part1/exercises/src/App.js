f596cb69daff01980766ca29c43c97ae724f6152
MrsKing2023 <smarshall84@yahoo.com>
2023-08-03 19:41:43 -0400

    fixed the error part 2

--- part1/exercises/src/App.js
+++ part1/exercises/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import MovieList from './components/MovieList.js';
 import ChoresList from './components/ChoresList.js';
 import BookList from './components/BookList.js';
+import HobbyLinks from './components/HobbyLinks.js';
+
 
 
 function App() {
