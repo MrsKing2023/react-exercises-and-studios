b74da20ba73d829abd7d66346b680df0e3e395a6
MrsKing2023 <smarshall84@yahoo.com>
2023-08-07 21:09:36 -0400

    studio 2

--- part2/studio/src/components/RateARecipe.js
+++ part2/studio/src/components/RateARecipe.js
@@ -1,7 +1,14 @@
 let stars = ["⭐", "⭐ ⭐", "⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐ ⭐"];
 
-function RateARecipe() {
+function RateARecipe(props) {
   return stars;
+
+  function GiveRating(props) {
+    return <h3>{stars[props.rating - 1]}</h3>;
+    <div> GiveRating = {`${stars} >= 1 && <= 5 === true ? [props.rating] : "null" `} </div>
+    //if (GiveRating)
+
+  }
 }
 
 export default RateARecipe;
